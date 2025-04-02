7091f411ee85b97465feb2a1b16b3592412e09a9
M373746 <j.beskow@sistran.com.br>
2025-04-02 15:40:51 -0300

    inclusao de metodo de gerar word

--- frontend-angular/fortify-trainer-ui/src/app/services/api.service.ts
+++ frontend-angular/fortify-trainer-ui/src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Apontamento } from '../models/apontamento.model';
 import { Observable } from 'rxjs';
@@ -6,11 +6,16 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class ApiService {
   private readonly baseUrl = 'http://localhost:8081';
-
+  private apiUrl = 'http://localhost:8081/api/falsos-positivos/gerar-relatorio';
   constructor(private http: HttpClient) {}
 
   sugerir(payload: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/sugerir-correcao/corrigir`, payload);
+  }
+
+  gerarRelatorio(file: FormData): Observable<Blob> {
+    const headers = new HttpHeaders();
+    return this.http.post(this.apiUrl, file, { headers, responseType: 'blob' });
   }
 
 }
